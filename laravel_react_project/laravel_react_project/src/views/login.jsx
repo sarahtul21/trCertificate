2dcdca1473c4961705dc2b58d2c2532af1750ca7
sarahtul21 <90712937+sarahtul21@users.noreply.github.com>
2024-09-05 15:12:04 +0300

    Update login.jsx

--- laravel_react_project/laravel_react_project/src/views/login.jsx
+++ laravel_react_project/laravel_react_project/src/views/login.jsx
@@ -41,9 +41,9 @@ export default function login(){
                     <input ref={emailRef} type="email" placeholder="Email" className='input input-accent w-full bg-[#f7f7f7] mb-3' />
                     <input ref={passwordRef} type="password" placeholder="Password" className='input input-accent w-full bg-[#f7f7f7] mb-3' />
                     <button className="btn btn-block btn-accent">Login</button>
-                    <p className="message">
+{/*                     <p className="message">
                     Not Registered? <Link to= '/register'>Create a new account</Link>
-                    </p>
+                    </p> */}
                 </form>
             </div>
         </div>
