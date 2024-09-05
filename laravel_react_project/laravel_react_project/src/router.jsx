6b782935da8161c537ff84cea033f773ce5023ac
sarahtul21 <90712937+sarahtul21@users.noreply.github.com>
2024-09-05 15:11:23 +0300

    Update router.jsx

--- laravel_react_project/laravel_react_project/src/router.jsx
+++ laravel_react_project/laravel_react_project/src/router.jsx
@@ -40,10 +40,10 @@ const router = createBrowserRouter ([
                 path: '/login',
                 element: <Login />,
             },
-            {
-                path: '/register',
-                element:  <Register />,
-            }
+            // {
+            //     path: '/register',
+            //     element:  <Register />,
+            // }
         ]
     },
 ]);
